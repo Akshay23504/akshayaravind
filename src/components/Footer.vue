e692d3d89626a145949bcafcfbf005bb07905cab
Akshay Aravind <Akshay23504@users.noreply.github.com>
2026-07-12 22:56:45 +0000

    Remove navbars...Keep it simple Adjust footer links and 404 text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-10 mb-6 prose m-auto flex slide-enter animate-delay-1200! copyright">
-    <span class="text-sm op50">Forked from <a target="_blank" href="https://antfu/antfu.me" style="color:inherit">antfu/antfu.me</a></span>
+    <span class="text-sm op50">Forked from <a target="_blank" href="https://antfu.me" style="color:inherit">antfu.me</a></span>
     <div class="flex-auto" />
   </div>
 </template>
